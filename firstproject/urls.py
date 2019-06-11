da20489e473953838b1879290534a96c20af534f
heatspurple <soonsoo564@naver.com>
2019-06-11 21:59:48 +0900

    last modified, ready to submit

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -5,6 +5,7 @@ import wordcount.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', wordcount.views.home, name="home"),
+    path('home/', wordcount.views.home, name=""),
     path('about/', wordcount.views.about, name="about"),
     path('result/', wordcount.views.result, name="result"),
 
